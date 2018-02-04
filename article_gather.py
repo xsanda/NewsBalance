6dfd93550fe9dfc97063393a2c579cf3b552069c
Amartya Vadlamani <amartya.vadlamani@gmail.com>
2018-02-04 04:16:40 +0000

    about as good as we are going to get

--- article_gather.py
+++ article_gather.py
@@ -1,4 +1,4 @@
-
+import urllib
 import newspaper
 import requests
 import json
@@ -28,31 +28,37 @@ def download(post):
             'text' : article.text,
             'summary' : article.text[:100] + '...'
         }
-        print(articledict)
+        #print(articledict)
         return articledict
     except Exception as e:
-        print(e)
+        pass#print(e)
 #http://webhose.io/filterWebContent?token=f063d591-11ac-43ae-842d-0814f86a6609&format=json&sort=crawled&q=Brexit%20language%3Aenglish%20site_type%3Anews%20domain_rank%3A%3C1000%20performance_score%3A%3E3%20published%3A%3E1514764800000%20spam_score%3A%3C0.8
 
 def article_gather(keyword):
     "The function returns a JSON object of the scraped data from the 20 most relevant news sources"
     
-    newsApiUrl = 'http://webhose.io/filterWebContent?token=f063d591-11ac-43ae-842d-0814f86a6609&format=json&sort=crawled&q=' + keyword + '%20language%3Aenglish%20site_type%3Anews%20domain_rank%3A%3C1000%20performance_score%3A%3E3%20published%3A%3E1514764800000%20spam_score%3A%3C0.8'
+    newsApiUrl = 'http://webhose.io/filterWebContent' 
     
     options = {
         "token": "f063d591-11ac-43ae-842d-0814f86a6609",
-        "q": keyword,
-        "language": "english",
-        "site_type": "news",
-        "domain_rank": "<1000",
-        "performance_score": ">3",
-        "published": ">1514764800000",
-        "spam_score": "<0.8"
+        "sort": "relevancy",
+        "format": "json",
+        "ts": "1515118211219",
+        "q": keyword+' '
+            'language:english '
+            'site_type:news '
+            'domain_rank:<500 '
+            'performance_score:>6 '
+            'published:>1514764800000 '
+            'spam_score:<0.25'
     }
-
-    response = requests.get(newsApiUrl)
+    qsa = "&".join(key + "=" + urllib.parse.quote(value) for key,value in options.items())
+    print(newsApiUrl+'?'+qsa)
+    response = requests.get(newsApiUrl+'?'+qsa)
     data = response.json()
     newsArticles = []
+    data['posts'] = data['posts'][:20]
+    print(data)
     with Pool(len(data['posts'])) as p:
         newsArticles = p.map(download, data['posts'])
 
--- poc.py
+++ poc.py
@@ -1,8 +1,11 @@
 import numpy
 import nltk
 import scipy.spatial
+from scipy.cluster.vq import kmeans
 import itertools
 from gensim.models import KeyedVectors
+
+dist = scipy.spatial.distance.euclidean
 
 wordvec = KeyedVectors.load_word2vec_format("./models/GoogleNews-vectors-negative300.bin", binary=True)
 
@@ -10,6 +13,10 @@ def doc2vec(doc):
     return sum(wordvec.get_vector(word) for word in nltk.word_tokenize(doc) if word in wordvec) 
 
 def polarize(docs):
-    docs = [doc for doc in docs if len(nltk.word_tokenize(doc["text"])) > 50 and doc != None]
-    furthest = max(itertools.combinations(docs, 2), key = lambda doc_1_2: scipy.spatial.distance.cosine(doc2vec(doc_1_2[0]["text"]), doc2vec(doc_1_2[1]["text"])))
-    return list(sorted(docs, key=lambda doc: scipy.spatial.distance.cosine(doc2vec(doc["text"]), doc2vec(furthest[0]["text"]))))
+
+    docs = [(doc, doc2vec(doc["text"])) for doc in docs if doc != None]#and len(nltk.word_tokenize(doc["text"])) > 50}
+    codebook, distortion = kmeans(numpy.asarray(list(map(lambda x: x[1], docs))), 2, iter=40)
+    
+    return [
+            list(doc for doc, v in sorted([(doc,vec) for doc, vec in docs if dist(vec, codebook[0]) > dist(vec, codebook[1])], key=lambda dv: dist(dv[1],codebook[1]))),
+            list(doc for doc, v in sorted([(doc,vec) for doc, vec in docs if dist(vec, codebook[0]) < dist(vec, codebook[1])], key=lambda dv: dist(dv[1],codebook[0])))]
